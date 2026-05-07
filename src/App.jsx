f1fcc0412c8488f8c3d055af6e24d6129cbc0f73
iamsarb001 <sarbbsandhu555@gmail.com>
2026-05-07 21:44:07 +0530

    tailwindcss installed & working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import './App.css'
+import './App.css';
 import { Sidebar } from './components/Sidebar'
 import { Mainview } from './components/Mainview'
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 export  const  Sidebar = () => {
     return (
         <>
+         <div className="text-3xl bg-red-600">
+             Hello inside Sidbar 
+         </div>
          Sidebar Section 
         </>
     )
