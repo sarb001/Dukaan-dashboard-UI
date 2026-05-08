4dfae5710cba547a6d3416cec445a4cdcd1b072d
iamsarb001 <sarbbsandhu555@gmail.com>
2026-05-08 06:38:41 +0530

    refactor : render logos dynamically

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 
-import { Analyticsimg, Appearancesimg, Audienceimg, Deliveryimg, Discountsimg, Homeimg, Marketingimg, Ordersimg, Payoutsimg, Pluginsimg, Productimg } from "../images/Allimages";
+import { Analyticsimg, Appearancesimg, Audienceimg, Deliveryimg, Discountsimg, Dropdownimg, Homeimg, Marketingimg, Ordersimg, Payoutsimg, Pluginsimg, Productimg, Walletimg } from "../images/Allimages";
 
 import Nishayanimg from '../assets/nishyanpic.webp';
 
@@ -22,6 +22,8 @@ export  const  Sidebar = () => {
     return (
         <>
          <div className="bg-[#1e2640]  text-white">
+
+
               <div>
                   <img src = {Nishayanimg}
                     width="18" height = "18" />
@@ -29,10 +31,34 @@ export  const  Sidebar = () => {
                     <span> Nishyan </span>   
                     <span> Visit  store </span>
                   </div>
-                  
+                  <div> <Dropdownimg /> </div>
               </div>
 
+            {/* options List  */}
+             <div>
+                     {OptionsList?.map(i => {
+                       return (
+                        <div className="grid grid-cols-[2fr,6fr] pt-2 pb-2 ">
+                           <div> {i?.logo} </div>
+                           <div>  {i?.name} </div> 
+                        </div>
+                        )
+                     })}
+                     {/* {OptionsList?.map(i => (
+                        <>
+                          <div>  {i?.logo} </div> 
+                          <div>  {i?.name} </div> 
+                        </>
+                     ))} */}
+             </div>
               
+              <div>
+                <div> <Walletimg /> </div>
+                <div>
+                    <div> Available credits </div>
+                    <span> 222.10 </span>
+                </div>
+              </div>
          </div>
         </>
     )
