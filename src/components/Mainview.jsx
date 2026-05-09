5217373f145eaa369657befa0fa14a683872b901
iamsarb001 <sarbbsandhu555@gmail.com>
2026-05-09 23:12:55 +0530

    refactor - Headerbar completed

--- src/components/Mainview.jsx
+++ src/components/Mainview.jsx
@@ -1,7 +1,44 @@
+import { Magnifyglassicon, Questionimg, SpeakerIcon, ToggleIcon } from "../images/Allimages"
+
 export  const  Mainview = () => {
     return (
         <>
+         <Header />
          MAIN VIEW  Section 
         </>
     )
+}
+
+export  const Header  = () => {
+     return (
+          <div className="grid grid-cols-[2.5fr,4fr,1fr]  h-[14vh] pl-4 pr-4"> 
+
+            <div className="grid grid-cols-[2fr,4fr]">
+               <div className="text-xl items-center flex"> Payouts </div>
+
+                <div className="grid grid-cols-[1fr,6fr] justify-start items-center  "> 
+                    <div className="flex items-center pl-2"> <Questionimg /> </div> 
+                    <div className="text-[14px]"> How it work  </div>
+                </div>
+
+            </div>
+
+            <div className = "p-3">
+                <div className="bg-[#f2f2f2] grid grid-cols-[1fr,8fr] p-2 rounded-[4px] w-72 ">
+                  <div className="flex items-center "> <Magnifyglassicon /> </div>
+                  <div className="flex">
+                    <input type = "text" placeholder="Search features,tutorials,etc." 
+                     className="focus:outline-none"
+                    /> 
+                  </div>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-2 items-center">
+                    <div> <SpeakerIcon /> </div>   
+                    <div> <ToggleIcon /> </div>   
+            </div>
+
+          </div>
+     )
 }
