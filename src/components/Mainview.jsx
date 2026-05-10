0ce9e3ca7009c721ef37aaf4585fa2802b7a7d0b
iamsarb001 <sarbbsandhu555@gmail.com>
2026-05-10 07:48:16 +0530

    Listed: Cards-info listed

--- src/components/Mainview.jsx
+++ src/components/Mainview.jsx
@@ -9,22 +9,6 @@ export  const  Mainview = () => {
     )
 }
 
-export const MainSection = () => {
-    return (
-        <>
-           <div className = "bg-[#f1f5f9]">
-             <div className = "grid grid-cols-2">
-                <div> Overview </div>
-                <div className="grid grid-cols-[1fr,1fr]">
-                    <div> Last Month </div>
-                    <div> <Dropdownimg color = "black" /> </div>
-                </div>
-             </div>
-           </div> 
-        </>
-    )
-}
-
 export  const Header  = () => {
      return (
           <div className="grid grid-cols-[2.5fr,4fr,1fr]  h-[14vh] pl-4 pr-4"> 
@@ -57,4 +41,58 @@ export  const Header  = () => {
 
           </div>
      )
+}
+
+export const MainSection = () => {
+
+    const Cardsinfo = [
+        { cardstext : "Next Payout"  , price : "2312.23" , orders  : 23 , Nextpayoutdate : "Today 04:00PM" },
+        { cardstext : "Next Payout"  , price : "92,312.20" , orders  : 13 , },
+        { cardstext : "Next Payout"  , price : "23,92,312.19" , orders  : "" , },
+    ]
+
+    const  TrnsxList = [
+        {
+            orderId :  "#281209", Status : "Successful" , TransactionID : "13163445747" , RefundDate : "Today,08:45 PM" , OrderAmount : "1125.00"
+        },
+        {
+            orderId :  "#281209", Status : "Processing" , TransactionID : "13163445747" , RefundDate : "Yesterday,3:00 PM" , OrderAmount : "1125.00"
+        },
+        {
+            orderId :  "#281209", Status : "Successful" , TransactionID : "13163445747" , RefundDate : "11 Jul 2023,03:00 PM" , OrderAmount : "1125.00"
+        },
+        {
+            orderId :  "#281209", Status : "Successful" , TransactionID : "13163445747" , RefundDate : "12 Jul 2023,04:00 AM" , OrderAmount : "1125.00"
+        },
+        {
+            orderId :  "#281209", Status : "Successful" , TransactionID : "13163445747" , RefundDate : "12 Jul 2023,04:00 AM" , OrderAmount : "1125.00"
+        },
+        {
+            orderId :  "#281209", Status : "Successful" , TransactionID : "13163445747" , RefundDate : "12 Jul 2023,05:00 AM" , OrderAmount : "1125.00"
+        },
+        {
+            orderId :  "#281209", Status : "Processing" , TransactionID : "13163445747" , RefundDate : "13 Jul 2023,03:00 PM" , OrderAmount : "1125.00"
+        },
+        {
+            orderId :  "#281209", Status : "Successful" , TransactionID : "13163445747" , RefundDate : "12 Jul 2023,05:00 PM" , OrderAmount : "1125.00"
+        },
+    ]
+
+    return (
+        <>
+           <div className = "bg-[#f1f5f9]">
+
+             <div className = "grid grid-cols-2">
+                <div> Overview </div>
+                <div className="grid grid-cols-[1fr,1fr]">
+                    <div> Last Month </div>
+                    <div> <Dropdownimg color = "black" /> </div>
+                </div>
+             </div>
+
+                {/* Cards layout */}
+              <div>  Payouts </div>
+           </div> 
+        </>
+    )
 }
